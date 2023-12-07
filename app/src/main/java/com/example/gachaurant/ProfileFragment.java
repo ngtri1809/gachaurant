6efb06970e9e5e322547d07b24b2382c35323b27
OmJam <101439736+OmJam@users.noreply.github.com>
2023-12-06 17:38:13 -0800

    Profile

--- app/src/main/java/com/example/gachaurant/ProfileFragment.java
+++ app/src/main/java/com/example/gachaurant/ProfileFragment.java
@@ -1,12 +1,21 @@
 package com.example.gachaurant;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,12 +26,16 @@ public class ProfileFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
+    public static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    String userIDP;
+    FirebaseAuth fAuthP;
+
+    private  int checkInCount = 0;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -53,6 +66,7 @@ public class ProfileFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
@@ -60,5 +74,50 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+//        FirebaseUser currentuser = FirebaseAuth.getInstance().getCurrentUser();
+//        String name = currentuser.getEmail();
+//        TextView strName = view.findViewById(R.id.userName);
+//        strName.setText(name);
+        Button btnCheck = (Button) view.findViewById(R.id.buttonCheckIn);
+        Button btnReward = (Button) view.findViewById(R.id.buttonReward);
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //where is update value of progress?
+                checkIn();
+            }
+        });
+
+        btnReward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goReward();
+            }
+        });
+    }
+
+    private void checkIn(){
+        checkInCount++;
+        AlertDialog.Builder alterBuilder = new AlertDialog.Builder(getContext());
+        alterBuilder.setMessage("Thank you for checking in! +10 Experience!");
+        alterBuilder.setTitle("Check-in");
+        alterBuilder.setPositiveButton("OK", null);
+        AlertDialog dlg = alterBuilder.create();
+        dlg.show();
+
+    }
+    private void goReward(){
+        Bundle args = new Bundle();
+        args.putInt(ARG_PARAM1, checkInCount);
+        Fragment rewardFragment = new RewardsFragment();
+        rewardFragment.setArguments(args);
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, rewardFragment);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/example/gachaurant/RewardsFragment.java
+++ app/src/main/java/com/example/gachaurant/RewardsFragment.java
@@ -1,5 +1,6 @@
 package com.example.gachaurant;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +29,9 @@ public class RewardsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private int checkCount;
+
 
     public RewardsFragment() {
         // Required empty public constructor
@@ -50,8 +59,9 @@ public class RewardsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            checkCount = getArguments().getInt(ARG_PARAM1);
+            //mParam1 = getArguments().getString(ARG_PARAM1);
+            //mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
@@ -60,5 +70,42 @@ public class RewardsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_rewards, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        ProgressBar bar = view.findViewById(R.id.progressBarReward);
+        TextView strBar = view.findViewById(R.id.currentProgress);
+        int nprog = checkCount%10 * 10;
+        strBar.setText(String.valueOf(nprog) + "/100");
+        bar.setProgress(nprog);
+        Button btnRedeem = (Button) view.findViewById(R.id.buttonRedeem);
+        btnRedeem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //where is update value of progress?
+                redeemMessage();
+            }
+        });
+    }
+
+    private void redeemMessage(){
+        if(checkCount%10*10 > 99){
+            AlertDialog.Builder alterBuilder = new AlertDialog.Builder(getContext());
+            alterBuilder.setMessage("Congrats Enjoy Your 5$ Reward");
+            alterBuilder.setTitle("Congratulations");
+            alterBuilder.setPositiveButton("OK", null);
+            AlertDialog dlg = alterBuilder.create();
+            dlg.show();
+        }
+        else {
+            AlertDialog.Builder alterBuilder = new AlertDialog.Builder(getContext());
+            alterBuilder.setMessage("You Experience must be 100 to Redeem Reward");
+            alterBuilder.setTitle("Experience Low");
+            alterBuilder.setPositiveButton("OK", null);
+            AlertDialog dlg = alterBuilder.create();
+            dlg.show();
+        }
+
     }
 }
