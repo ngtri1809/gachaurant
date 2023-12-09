5eb82e64faaa6ce647d6e98875f72e3d43e7ed66
OmJam <101439736+OmJam@users.noreply.github.com>
2023-12-09 03:53:41 -0800

    checkin and rewards done locally on app, format changes

--- app/src/main/java/com/example/gachaurant/RewardsActivity.java
+++ app/src/main/java/com/example/gachaurant/RewardsActivity.java
@@ -8,14 +8,26 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
 
 public class RewardsActivity extends AppCompatActivity {
+    Button backButtonReward;
     public static final String CHECK_IN_COUNT_KEY = "checkInCount";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rewards);
+        backButtonReward = findViewById(R.id.backBtn6);
+
+        // Set click listeners for each button
+        backButtonReward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         int checkInCount = sharedPreferences.getInt(CHECK_IN_COUNT_KEY, 0);
